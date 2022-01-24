7b80333b429bf675c0e034c60bef22cd033dfe23
gityasaman <shokri.yasaman@yahoo.com>
2022-01-24 17:10:11 +0330

    registered Answer and Question model in django admin

--- questions/admin.py
+++ questions/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Answer, Question
 
-# Register your models here.
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ['title', ]
+    list_filter = ('title', 'created')
+    search_fields = ('title', 'body')
+    prepopulated_fields = {'slug': ('title',)}
+
+@admin.register(Answer)
+class AnswerAdmin(admin.ModelAdmin):
+    list_display = ['body', 'question_id']
