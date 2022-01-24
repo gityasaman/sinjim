a66377674027c1eeafbf71a0e476cc81465a7d76
gityasaman <shokri.yasaman@yahoo.com>
2022-01-24 17:09:13 +0330

    modified Answer and Question models

--- questions/models.py
+++ questions/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from account.models import MyUser
 from taggit.managers import TaggableManager
 
 class Question(models.Model):
@@ -7,9 +8,10 @@ class Question(models.Model):
     body            = models.TextField(blank=True)
     created         = models.DateTimeField(auto_now_add=True)
     questioner      = models.ForeignKey(MyUser, on_delete=models.CASCADE)
-    verified_answer = models.ForeignKey(Answer, on_delete=models.CASCADE)
-    upvotes         = models.IntegerField()
+    upvotes         = models.ManyToManyField(MyUser, related_name='voters', blank=True)
     tags            = TaggableManager()
+    verified_answer = models.ForeignKey('questions.Answer', on_delete=models.CASCADE, blank=True, null=True)
+
 
     def __str__(self):
         return self.title
@@ -19,4 +21,5 @@ class Answer(models.Model):
     question_id     = models.ForeignKey(Question, on_delete=models.CASCADE)
     body            = models.TextField()
     created         = models.DateTimeField(auto_now_add=True)
-    upvotes         = models.IntegerField()
+    upvotes         = models.ManyToManyField(MyUser, related_name='upvoters')
+    downvotes       = models.ManyToManyField(MyUser, related_name='downvoters')
