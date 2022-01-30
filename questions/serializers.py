4d7804fdf79b63cd527486f0e762d382eb14a247
gityasaman <shokri.yasaman@yahoo.com>
2022-01-30 13:31:07 +0330

    added answers field to AnswerSerializer

--- questions/serializers.py
+++ questions/serializers.py
@@ -2,16 +2,19 @@ from rest_framework import serializers
 from .models import Question, Answer
 from taggit_serializer.serializers import TaggitSerializer, TagListSerializerField
 
-class QuestionSerializer(serializers.ModelSerializer, TaggitSerializer):
-    tags = TagListSerializerField()
-
-    class Meta:
-        model = Question
-        fields = ['title', 'body', 'tags']
-
 class AnswerSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Answer
         fields = ['body']
+
+
+class QuestionSerializer(serializers.ModelSerializer, TaggitSerializer):
+    tags = TagListSerializerField()
+    answers = serializers.StringRelatedField(many=True)
+
+    class Meta:
+        model = Question
+        fields = ['title', 'body', 'tags', 'answers']
+
 
