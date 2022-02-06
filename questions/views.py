119295dc8a91e7336b5b73462f292eb001f5a508
gityasaman <shokri.yasaman@yahoo.com>
2022-02-06 14:13:54 +0330

    first approach for verifying answers

--- questions/views.py
+++ questions/views.py
@@ -3,7 +3,7 @@ from rest_framework import generics
 from rest_framework.views import APIView, View
 from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
 from .models import Question, Answer
-from .serializers import QuestionSerializer, AnswerSerializer
+from .serializers import QuestionSerializer, AnswerSerializer, VerifyAnswerSerializer
 from rest_framework.response import Response
 from django.contrib.auth import get_user_model
 
@@ -26,24 +26,26 @@ class QuestionListView(generics.ListAPIView):
 
 class QuestionDetailView(APIView):
     user = get_user_model()
-    serializer_class = AnswerSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
 
     def get(self, request, pk):
         question = get_object_or_404(Question, pk=pk)
         serializer = QuestionSerializer(question)
-        return Response({'question': serializer.data})
+        return Response({'question': serializer.data}) 
     
     def post(self, request, pk):
         question_id = Question.objects.get(pk=pk)
-        serializer = self.serializer_class(data=request.data)
-        if serializer.is_valid(): 
-            serializer.save(answerer_id=request.user.id, question_id=question_id)   
-            return Response(serializer.data)
+        if request.user == question_id.questioner:
+            serializer = AnswerSerializer(data=request.data)
+            if serializer.is_valid():
+                serializer.save(answerer_id=request.user.id, question_id=question_id)   
+                return Response(serializer.data)
+            else:
+                return Response(serializer.errors)
         else:
-            return Response(serializer.errors)
+            serializer = VerifyAnswerSerializer(data=request.data)
 
-class UpvoteView(APIView):
+class UpvoteQuestionView(APIView):
     user = get_user_model()
 
     def get(self, request, pk):
