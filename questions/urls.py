27b97a9373fb4d66a168dc53d8c787e45b5d7a84
gityasaman <shokri.yasaman@yahoo.com>
2022-02-07 15:56:35 +0330

    added edit questions url

--- questions/urls.py
+++ questions/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('', views.QuestionListView.as_view(), name='questions_list'),
     path('<pk>', views.QuestionDetailView.as_view(), name='question_detail'),
     path('<pk>/upvote', views.UpvoteQuestionView.as_view(), name='upvote'),
+    path('<pk>/edit', views.QuestionUpdateView.as_view(), name='edit'),
+
 ]
