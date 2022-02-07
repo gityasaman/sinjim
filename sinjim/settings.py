ced3ea1b52ab33dc222accbefbb0f9fd77a7462f
gityasaman <shokri.yasaman@yahoo.com>
2022-02-07 15:48:00 +0330

    add cors-headers

--- sinjim/settings.py
+++ sinjim/settings.py
@@ -56,6 +56,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -127,6 +128,8 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
+
+CORS_ORIGIN_ALLOW_ALL = True
 
 CACHES = {
     "default": {
