65b734d6faddcfb141f7e7c05bdec4c31d6a6c7c
gityasaman <shokri.yasaman@yahoo.com>
2022-02-12 20:43:13 +0330

    added upvote, downvote and answer detail urls

--- questions/urls.py
+++ questions/urls.py
@@ -8,5 +8,8 @@ urlpatterns = [
     path('<pk>', views.QuestionDetailView.as_view(), name='question_detail'),
     path('<pk>/upvote', views.UpvoteQuestionView.as_view(), name='upvote'),
     path('<pk>/edit', views.QuestionUpdateView.as_view(), name='edit'),
-
+    path('answer/<pk>', views.AnswerDetailView.as_view(), name='answer_detail'),
+    path('answer/<pk>/upvote', views.UpvoteAnswerView.as_view(), name='answer_upvote'),
+    path('answer/<pk>/downvote', views.DownvoteAnswerView.as_view(), name='answer_downvote'),
+    
 ]
