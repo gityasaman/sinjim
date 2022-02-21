4e7a55077a76c509f2c0e801d12fbb1bc7aa7ba7
gityasaman <shokri.yasaman@yahoo.com>
2022-02-21 10:48:13 +0330

    added search url

--- questions/urls.py
+++ questions/urls.py
@@ -3,6 +3,7 @@ from rest_framework import routers
 from django.urls import path, include
 
 urlpatterns = [
+    path('search', views.QuestionSearchView.as_view(), name='search'),
     path('ask', views.CreateQuestionView.as_view(), name='ask'),
     path('', views.QuestionListView.as_view(), name='questions_list'),
     path('<pk>', views.QuestionDetailView.as_view(), name='question_detail'),
@@ -10,6 +11,5 @@ urlpatterns = [
     path('<pk>/edit', views.QuestionUpdateView.as_view(), name='edit'),
     path('answer/<pk>', views.AnswerDetailView.as_view(), name='answer_detail'),
     path('answer/<pk>/upvote', views.UpvoteAnswerView.as_view(), name='answer_upvote'),
-    path('answer/<pk>/downvote', views.DownvoteAnswerView.as_view(), name='answer_downvote'),
-    
+    path('answer/<pk>/downvote', views.DownvoteAnswerView.as_view(), name='answer_downvote'),  
 ]
