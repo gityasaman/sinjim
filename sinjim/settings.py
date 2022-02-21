3b6d23f10c682b93030db5ad2489194eac4c9998
gityasaman <shokri.yasaman@yahoo.com>
2022-02-21 10:54:25 +0330

    added default rest framework pagination

--- sinjim/settings.py
+++ sinjim/settings.py
@@ -154,6 +154,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 REST_FRAMEWORK = {
+
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 10
+
     # 'DEFAULT_AUTHENTICATION_CLASSES': (
     #     'rest_framework.authentication.TokenAuthentication',
     # ),
