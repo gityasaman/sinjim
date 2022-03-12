b7d7a9b90d4594e2d7fb1ffde0d38e73dd33635e
gityasaman <shokri.yasaman@yahoo.com>
2022-03-13 00:27:35 +0330

    added continue register view and added email existance checking to emailview

--- account/views.py
+++ account/views.py
@@ -3,7 +3,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from django.http import HttpResponseRedirect
 from rest_framework import generics, permissions, status, viewsets, mixins
-from .serializers import  EmailSerializer, CodeSerializer
+from .serializers import  EmailSerializer, CodeSerializer, MyUserSerializer
 from oauth2_provider.contrib.rest_framework import TokenHasReadWriteScope, TokenHasScope
 from . import methods
 from django.conf import settings
@@ -11,6 +11,7 @@ import random
 import redis
 from django.conf import settings
 import hashlib
+from .models import MyUser
 
 r = redis.StrictRedis(host=settings.REDIS_HOST,
                                   port=settings.REDIS_PORT, db=0)
@@ -37,24 +38,16 @@ r = redis.StrictRedis(host=settings.REDIS_HOST,
 #             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-class RegisterView(APIView):
-
+class ContinueRegisterView(APIView):
+    serializer_class = MyUserSerializer
     def post(self, request, format=None):
-        serializer = RegisterSerializer(data=request.data)
-        data = {}
+        serializer = MyUserSerializer(data=request.data)
+        email = request.session['email']
         if serializer.is_valid():
-            user = serializer.save()
-            profile = Profile.objects.create(user=user)
-            data['response'] = 'Successfully registered'
-            data['email'] = user.email
-            data['username'] = user.username
-            data['avatar'] = user.avatar
+            user = serializer.save(email)
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-               
-    
 
 class EmailView(APIView):
     serializer_class = EmailSerializer
@@ -64,31 +57,30 @@ class EmailView(APIView):
         if serializer.is_valid():
             code = methods.generate_code()
             email = serializer.validated_data['email']
-            request.session['email'] = email
-            r.set(email, code)
-            r.expire(email, 300)
-            if r.exists(email):
-                methods.send_verification_mail(email, code)
-                return HttpResponseRedirect('code')
-
+            if MyUser.objects.filter(email=email).count():
+                return Response({"message": "user already registered"})
+            else:
+                request.session['email'] = email
+                r.set(email, code)
+                r.expire(email, 300)
+                if r.exists(email):
+                    methods.send_verification_mail(email, code)
+                    return HttpResponseRedirect('code')
         else:
-            return serializer.errors
-
+            return Response(serializer.errors)
 
 class CodeView(APIView):
     serializer_class = CodeSerializer
 
     def post(self, request, format=None):
         serializer = CodeSerializer(data=request.data)
-        
         if serializer.is_valid():
             email = request.session['email']
             redis_code = r.get(email)  #Get the verification code from redis
             redis_code = redis_code.decode("utf-8")  #delete the b'' from the string
             if  serializer.validated_data['code'] == redis_code:
-                return HttpResponseRedirect('continue_register') 
+                return HttpResponseRedirect('continue-register') 
             else:
                 return Response({"message":'codes does not match'})
         else:
             return serializer.errors
-
