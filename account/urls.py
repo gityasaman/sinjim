b4c70e48073bf15d19ef9a1f3de07fe26c5957d7
gityasaman <shokri.yasaman@yahoo.com>
2022-03-13 00:26:22 +0330

    changed some urls and added continue register url

--- account/urls.py
+++ account/urls.py
@@ -2,7 +2,7 @@ from django.contrib.auth import views as auth_views
 from django.urls import path, include
 from . import views
 from rest_framework.routers import DefaultRouter
-
+from rest_framework.authtoken.views import obtain_auth_token
 
 app_name = 'account'
 
@@ -13,8 +13,10 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('', include(router.urls)),
-    path('email/', views.EmailView.as_view(), name='email'),
-    path('email/code', views.CodeView.as_view(), name='email_code'),
+    path('register/', views.EmailView.as_view(), name='register'),
+    path('register/code', views.CodeView.as_view(), name='email_code'),
+    path('register/continue-register', views.ContinueRegisterView.as_view(), name='continue_register'),
+    path('api-token-auth', obtain_auth_token),
     #path('code', views.CodeView.as_view(), name='code')
     #path('register/email/', views.EmailCodeView.as_view(), name='email'),
     #path('users/', views.UserList.as_view(), name='users'),
