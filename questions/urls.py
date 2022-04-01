6d004c6f3ad362ef9a0992a4fdf6d4dfac9408a9
gityasaman <shokri.yasaman@yahoo.com>
2022-04-01 12:52:42 +0430

    modified question detail path

--- questions/urls.py
+++ questions/urls.py
@@ -1,6 +1,7 @@
 from . import views
-from django.urls import path, include
+from django.urls import path, include, register_converter
 from rest_framework import routers
+from sinjim.urls import PersianSlugConverter
 
 router = routers.DefaultRouter()
 router.register('', views.TagViewSet, basename='tags')
@@ -12,9 +13,9 @@ urlpatterns = [
     path('search', views.QuestionSearchView.as_view(), name='question_search'),
     path('tags/search', views.TagSearchView.as_view(), name='tag_search'),
     path('ask', views.CreateQuestionView.as_view(), name='ask'),
+    path('<int:pk>', views.QuestionDetailView.as_view(), name='question_detail'),
+    path('<persian_slug:tag_slug>', views.QuestionListView.as_view(), name='questions_by_tag'),
     path('', views.QuestionListView.as_view(), name='questions_list'),
-    path('<slug:tag_slug>', views.QuestionListView.as_view(), name='questions_by_tag'),
-    path('<pk>', views.QuestionDetailView.as_view(), name='question_detail'),
     path('<pk>/upvote', views.UpvoteQuestionView.as_view(), name='upvote'),
     path('<pk>/edit', views.QuestionUpdateView.as_view(), name='edit'),
     path('answer/<pk>', views.AnswerDetailView.as_view(), name='answer_detail'),
