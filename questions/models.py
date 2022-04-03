0a524ca378ac7b67202343f50c9da4cd6170fdcd
gityasaman <shokri.yasaman@yahoo.com>
2022-04-03 22:38:14 +0430

    added two models for tag customization to support persian language

--- questions/models.py
+++ questions/models.py
@@ -1,16 +1,39 @@
 from django.db import models
 from account.models import MyUser
 from taggit.managers import TaggableManager
+from taggit.models import Tag, TagBase, TaggedItemBase, GenericTaggedItemBase
 from django.utils.text import slugify
+from django.utils.translation import pgettext_lazy
+from django.utils.translation import gettext_lazy as _
+from django.db.models.signals import post_save
+
+class QuestionTag(TagBase):
+    slug = models.SlugField(allow_unicode=True)
+
+    class Meta:
+        verbose_name = _("Tag")
+        verbose_name_plural = _("Tags")
+
+    def save(self, *args, **kwargs):
+        value = self.name
+        self.slug = slugify(value, allow_unicode=True)
+        super().save(*args, **kwargs)
+
+class TaggedQuestion(GenericTaggedItemBase):
+    tag = models.ForeignKey(
+        QuestionTag,
+        on_delete=models.CASCADE,
+        related_name="%(app_label)s_%(class)s_items",
+    )
 
 class Question(models.Model):
     title           = models.CharField(max_length=160)
-    slug            = models.CharField(unique=True, max_length=255)
+    slug            = models.SlugField(unique=True, max_length=255, allow_unicode=True)
     body            = models.TextField(blank=True)
     created         = models.DateTimeField(auto_now_add=True)
     questioner      = models.ForeignKey(MyUser, on_delete=models.CASCADE)
     upvoters        = models.ManyToManyField(MyUser, related_name='upvotes', blank=True)
-    tags            = TaggableManager()
+    tags            = TaggableManager(through=TaggedQuestion)
     verified_answer = models.OneToOneField('questions.Answer', on_delete=models.CASCADE, blank=True, null=True)
 
     def __str__(self):
