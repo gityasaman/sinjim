21499127949358b7e48c59006b611889b80831cc
gityasaman <shokri.yasaman@yahoo.com>
2022-04-18 03:31:52 +0430

    modified tags field

--- questions/models.py
+++ questions/models.py
@@ -1,12 +1,11 @@
 from django.db import models
 from account.models import MyUser
 from taggit.managers import TaggableManager
-from taggit.models import Tag, TagBase, TaggedItemBase, GenericTaggedItemBase
+from taggit.models import TagBase, GenericTaggedItemBase
 from django.utils.text import slugify
-from django.utils.translation import pgettext_lazy
 from django.utils.translation import gettext_lazy as _
-from django.db.models.signals import post_save
 
+#Custom tag for supporting unicode
 class QuestionTag(TagBase):
     slug = models.SlugField(allow_unicode=True)
 
@@ -19,6 +18,7 @@ class QuestionTag(TagBase):
         self.slug = slugify(value, allow_unicode=True)
         super().save(*args, **kwargs)
 
+#Custom tag for supporting unicode
 class TaggedQuestion(GenericTaggedItemBase):
     tag = models.ForeignKey(
         QuestionTag,
@@ -33,7 +33,7 @@ class Question(models.Model):
     created         = models.DateTimeField(auto_now_add=True)
     questioner      = models.ForeignKey(MyUser, on_delete=models.CASCADE)
     upvoters        = models.ManyToManyField(MyUser, related_name='upvotes', blank=True)
-    tags            = TaggableManager(through=TaggedQuestion)
+    tags            = TaggableManager(through=TaggedQuestion, blank=True)
     verified_answer = models.OneToOneField('questions.Answer', on_delete=models.CASCADE, blank=True, null=True)
 
     def __str__(self):
